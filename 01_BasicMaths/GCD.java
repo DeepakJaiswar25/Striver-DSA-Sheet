01b75bbb284e1a43aff9319f0adb1a92a0217aba
DeepakJaiswar25 <139800079+DeepakJaiswar25@users.noreply.github.com>
2025-07-24 23:38:53 -0500

    GCD

---
+++ 01_BasicMaths/GCD.java
@@ -0,0 +1,18 @@
+
+
+public class GCD {
+public static void main(String[] args) {
+    int N1= 20;
+    int N2=15;
+   int gcd=1;
+   for(int i=1;i<=Math.min(N1,N2);i++){
+    if(N1%i==0 && N2%i==0){
+        gcd=i;
+    }
+   }
+   System.out.println("GCD : "+gcd);
+}    
+
+
+
+}
