d0048130d1af14172a2ec9c5787aa019959cde7b
DeepakJaiswar25 <139800079+DeepakJaiswar25@users.noreply.github.com>
2025-08-10 23:41:58 -0500

    ReverseArray

---
+++ 03_BasicRecursion/ReverseArray.java
@@ -0,0 +1,27 @@
+package BasicRecursion;
+
+import java.util.Arrays;
+
+public class ReverseArray {
+public static void main(String[] args) {
+    int[] arr={1,2,3,4,5,6};
+    int n=arr.length;
+    reverse(arr, 0, n-1);
+    System.out.println(Arrays.toString(arr));
+
+}
+public static void reverse(int[] arr,int l,int r){
+
+ if(l>=r){
+return;
+ }
+ swap(arr, l,r);
+ reverse(arr, l+1, r-1);
+}
+
+public static void swap(int[] arr,int l,int r){
+    int s= arr[l];
+    arr[l]=arr[r];
+    arr[r]=s;
+}
+}
