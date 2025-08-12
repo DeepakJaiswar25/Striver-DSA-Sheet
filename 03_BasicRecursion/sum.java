1e18d6c99c69dd2e46622d50341924b4f8580f22
DeepakJaiswar25 <139800079+DeepakJaiswar25@users.noreply.github.com>
2025-08-11 23:44:08 -0500

    sum

---
+++ 03_BasicRecursion/sum.java
@@ -0,0 +1,17 @@
+package BasicRecursion;
+
+public class sum {
+    public static void main(String[] args) {
+        int n=5;
+    int sum1toN = sum1toN(n);
+    System.out.println("sum: "+sum1toN);
+    }
+
+    public static int sum1toN(int i){
+        
+       if(i==0)
+       return 0;
+       
+       return i+sum1toN(i-1);
+    }
+}
