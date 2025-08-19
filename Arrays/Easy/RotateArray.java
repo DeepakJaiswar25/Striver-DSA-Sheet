05f847c0fda9d350fb46ba125c2c700276043ba4
DeepakJaiswar25 <139800079+DeepakJaiswar25@users.noreply.github.com>
2025-08-18 23:28:33 -0500

    RotateArray

---
+++ Arrays/Easy/RotateArray.java
@@ -0,0 +1,19 @@
+package ArrayPracticeEasy;
+
+import java.util.Arrays;
+
+public class RotateArray {
+    public static void main(String[] args) {
+        int[] arr= {1,2,3,4,5,6,7};
+        rotate(arr, arr.length);
+        System.out.println(Arrays.toString(arr));
+    }
+
+    public static void rotate(int[] arr,int k){
+       int temp= arr[0];
+       for(int i=0;i<k-1;i++){
+        arr[i]=arr[i+1];
+       }
+       arr[k-1]=temp;
+    }
+}
