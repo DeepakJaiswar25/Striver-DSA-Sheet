23af1a2656e37b295564b95159d32dcadaf5082d
DeepakJaiswar25 <139800079+DeepakJaiswar25@users.noreply.github.com>
2025-09-30 23:13:50 -0500

    Lower Bound in a Sorted Array

---
+++ BinarySearch/LowerBound.java
@@ -0,0 +1,28 @@
+package BinarySearch;
+
+public class LowerBound {
+    public static void main(String[] args) {
+        int[] arr = {3,5,8,15,19};
+         int x = 9;
+         int n= arr.length;
+         int ans=lowerBound(arr, n, x);
+         System.out.println(ans);
+    }
+     public static int lowerBound(int []arr, int n, int x) {
+        int low= 0;
+        int high= n-1;
+        int ans=n;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(arr[mid]>=x){
+                ans=mid;
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+    return ans;
+    }
+
+}
