7ca6173f70cd67e560a2570204cdaa05e82154d4
DeepakJaiswar25 <139800079+DeepakJaiswar25@users.noreply.github.com>
2026-03-21 23:33:44 -0500

    Implement Valid Parenthesis checker in Java

---
+++ Blind75/ValidParanthesis.java
@@ -0,0 +1,35 @@
+import java.util.Stack;
+
+public class ValidParanthesis {
+
+    public static void main(String[] args) {
+        String s = "([{}])";
+        boolean ans=isValid(s);
+        System.out.println(ans);
+    }
+
+    public static boolean isValid(String s) {
+        
+        Stack<Character> st= new Stack<>();
+        int n= s.length();
+        for(int i=0;i<n;i++){
+            char ch= s.charAt(i);
+           if(ch=='{' || ch=='(' || ch == '['){
+            st.push(ch);
+           }
+           else{
+            if (st.isEmpty()) return false;
+            char top= st.peek();
+            if(top=='{' && ch=='}' || top=='[' && ch==']' || top=='(' && ch==')'){
+                st.pop();
+            }
+            else{
+                return false;
+            }
+           }
+
+        }      
+        return st.isEmpty();
+    }
+    
+}
