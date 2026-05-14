f26b85c063bf826ada1016627e5f5b61cce64ff9
DeepakJaiswar25 <139800079+DeepakJaiswar25@users.noreply.github.com>
2026-05-13 23:52:33 -0500

    Implement HouseRobber solution in Java

---
+++ Blind75/HouseRobber.java
@@ -0,0 +1,20 @@
+public class HouseRobber {
+    public static void main(String[] args) {
+        int[] nums = {1,1,3,3};
+        int ans= rob(nums);
+        System.out.println(ans);
+    }
+
+    public static int rob(int[] nums) {
+        int n=nums.length;
+        if(n==1) return nums[0];
+        if(n==2) return Math.max(nums[1],nums[0]);
+        int[] dp= new int[n];
+        dp[0]=nums[0];
+        dp[1]= Math.max(nums[1],nums[0]);
+        for(int i=2;i<n;i++){
+            dp[i]=Math.max((dp[i-2]+nums[i]),dp[i-1]);
+        }
+         return dp[n-1];
+    }
+}
