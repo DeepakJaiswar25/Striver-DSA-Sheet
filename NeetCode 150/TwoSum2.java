d822da84b10963e7502533c1a91b4454537e04ee
DeepakJaiswar25 <139800079+DeepakJaiswar25@users.noreply.github.com>
2026-06-24 00:01:52 -0500

    Implement TwoSum2 algorithm in Java

---
+++ NeetCode 150/TwoSum2.java
@@ -0,0 +1,27 @@
+import java.util.Arrays;
+
+public class TwoSum2 {
+    public static void main(String[] args) {
+        int[] numbers = {1,2,3,4}; int target = 3;
+        int[] ans= twoSum(numbers, target);
+        System.out.println(Arrays.toString(ans));
+    }
+    public static int[] twoSum(int[] numbers, int target) {
+        int n= numbers.length;
+        int l= 0;
+        int r= n-1;
+        while(l<=r){
+            if(numbers[l]+numbers[r]==target){
+                return new int[]{l,r};
+            }
+            else if(numbers[l]+numbers[r]<target){
+                l++;
+            }
+            else{
+                r--;
+            }
+        }
+        
+        return new int[]{-1,-1};
+    }
+}
